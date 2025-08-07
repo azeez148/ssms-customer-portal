14556cc4d796d87b55dce30af566aaaa662c186d
azeez <azeez148@gmail.com>
2025-08-07 17:15:38 +0530

    minpr fix

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,7 +4,7 @@ import { ContactComponent } from './components/contact/contact.component';
 import { CustomerHomeComponent } from './customer-home/customer-home.component';
 
 export const routes: Routes = [
-    { path: '', redirectTo: '/customer-view', pathMatch: 'full' },
+    { path: '', redirectTo: '/about', pathMatch: 'full' },
     { path: 'about', component: AboutComponent },
     { path: 'contact', component: ContactComponent },
     {path: 'customer-view', component: CustomerHomeComponent },
